a601c8b58b12a2601f9392100bca17a536a6c3de
LUNTNT <93472532+LUNTNT@users.noreply.github.com>
2021-12-01 22:52:01 +0800

    .

--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import logo from './logo.png';
+import React, {Component} from 'react'
+import { Typography, Link } from "@material-ui/core";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+var headertextstyle = {
+    fontsize : 50,
+    fontFamily : "Noto Sans"
+}
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} alt="logo" height={150} width={150}/>
+                    <Typography style={headertextstyle}>
+                        Smart Pet Feeder
+                    </Typography>
+                </header>
+            </div>
+        )
+    }
+
 }
 
 export default App;
