e743e676bcb7aa2add834618a22cca7360832879
LUNTNT <93472532+LUNTNT@users.noreply.github.com>
2021-12-01 18:17:56 +0800

    h

--- Sweep/Sweep.ino
+++ Sweep/Sweep.ino
@@ -6,11 +6,23 @@
  by Scott Fitzgerald
  https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
 */
-
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 #include <Servo.h>
+#include "HX711.h"
+#include <Wire.h>
 
+ESP8266WiFiMulti WiFiMulti;
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
+
+HX711 scale(14, 12);   // 14 = ESP8266 GPIO14(D5), 12 = ESP8266 GPIO12(D6)
+float original_weight;
+//float calibrate_parameter = 338.13;  // this value is obtained by calibrating the scale with known weights; see the README for details
+float calibrate_parameter = 321.44;  // this value is obtained by calibrating the scale with known weights; see the README for details
 
 int pos = 0;    // variable to store the servo position
 int  inputPin  = 12;  // PIR OUT connected to GPIO 12(D6)
@@ -19,10 +31,18 @@ bool detect    = false;
 
 
 void setup() {
+  ESP.wdtDisable();
+  ESP.wdtFeed();
   Serial.begin(115200);
   myservo.attach(14);  
   pinMode (inputPin, INPUT);
   pinMode (led, OUTPUT);
+  Wire.begin(2,0);
+
+  scale.set_scale(calibrate_parameter);    // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();                           // reset the scale to 0
+
+  delay(500);
 }
 
 void loop() {
@@ -33,16 +53,19 @@ void loop() {
   if (detect == true) {
     digitalWrite(led, LOW);
     Serial.println("Movement detected");
-    Serial.println("Buzzer play");
-    /* Method - It creates a "normal distortion" effect on the Buzzer */
-    //    buzzer.distortion(NOTE_C3, NOTE_C5);
-    //    buzzer.distortion(NOTE_C5, NOTE_C3);
-
-    /* Method - It makes the Buzzer sound */
  
     myservo.write(180);  
     Serial.println(myservo.read());
-    delay(3000); 
+    delay(5000); 
+    myservo.write(0);
+    Serial.println(myservo.read());
+    delay(2000);
+
+    Serial.print("one reading: \t");
+    original_weight = scale.get_units(5);
+    Serial.print(original_weight, 1);
+    Serial.println(" g");
+
   }
   else {
     digitalWrite(led, HIGH);
